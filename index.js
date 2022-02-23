0f74cd357e45f22381dab9e5f706638c797982c6
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-02-23 20:43:21 +0100

    Update

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ client.on ("ready", () =>  {
     console.log ("Il bot è online!")
 })
 
+client.on('ready', () => {
+    client.user.setActivity('Closet of gamers', { type: 'PLAYING' }); 
+    client.user.setActivity("Closet of gamers", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/nomeutente"
+    });
+    //Stato online/offine/non disturbare... (Potrebbe volerci qualche tempo per doversi settare)
+    client.user.setStatus('online') //Oppure idle, dnd, invisible
+})
