0d5f53d8b5a34ee30b86a585136eff4322e95a1b
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-02-25 19:46:58 +0100

    Update

--- index.js
+++ index.js
@@ -54,3 +54,24 @@ client.on("messageCreate", message => {
         message.channel.send({ embeds: [embed] })
     }
 })
+client.on("messageCreate", message => {
+    if (message.content.startsWith("*clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        var count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("Inserisci un numero valido")
+        }
+        if (count > 100) {
+            return message.channel.send("Non puoi cancellare più di 100 messaggi")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati").then(msg => {
+            setTimeout(() => msg.delete(), 5000)
+        })
+    }
+})
