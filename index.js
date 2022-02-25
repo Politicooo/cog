5d3f1b17b325a27791da0dd76b459139778c1bf8
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-02-25 20:28:39 +0100

    Update

--- index.js
+++ index.js
@@ -97,3 +97,28 @@ client.on("messageCreate", message => {
         message.channel.send({ embeds: [embed] })
     }
 })
+client.on("messageCreate", message => {
+    if (message.channel.type == "DM") return
+
+    if (message.member.roles.cache.has("idRuolo1") || message.member.roles.cache.has("idRuolo2")) return
+
+    var parolacce = ["sedia", "lampada", "ciao come va"]
+    var trovata = false;
+    var testo = message.content;
+
+    parolacce.forEach(parola => {
+        if (message.content.toLowerCase().includes(parola.toLowerCase())) {
+            trovata = true;
+            testo = testo.replace(eval(`/${parola}/g`), "###");
+        }
+    })
+
+    if (trovata) {
+        message.delete();
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Hai detto una parolaccia")
+            .setDescription("Hai scritto un messaggio con parole bloccate\rIl tuo messaggio: " + testo)
+
+        message.channel.send({ embeds: [embed] })
+    }
+})
