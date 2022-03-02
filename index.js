c8f2221edc2425eaab9d9926318ef09225e2736b
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-03-02 19:29:10 +0100

    Upload

--- index.js
+++ index.js
@@ -7,11 +7,95 @@ client.login(process.env.token)
 client.on ("ready", () =>  {
     console.log ("Il bot è online!")
 })
-client.on('ready', () => {
-    client.user.setActivity('Closet of gamers', { type: 'PLAYING' });
-    client.user.setActivity("Closet of gamers", {
-    });
-    client.user.setStatus('online') 
+
+client.on("messageCreate", message => {
+    if (message.content.startsWith("*ban")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.bannable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.ban()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} bannato`)
+                    .setDescription(`Utente bannato da ${message.author.toString()}`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+client.on("messageCreate", async message => {
+    if (message.content.startsWith("*unban")) {
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+
+        var args = message.content.split(/\s+/);
+        var idUtente = args[1]
+
+        if (!idUtente) {
+            return message.channel.send("Non hai scritto l'id di nessun utente");
+        }
+
+        message.guild.members.unban(idUtente)
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle("Utente sbannato")
+                    .setDescription("Questo utente è stato sbannato")
+
+                message.channel.send({ embeds: [embed] })
+            })
+            .catch(() => { message.channel.send("Utente non valido o non bannato") })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("*kick")) {
+        var utente = message.mentions.members.first();
+        if (!message.member.permissions.has('KICK_MEMBERS')) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!utente) {
+            return message.channel.send('Non hai menzionato nessun utente');
+        }
+        if (!utente.kickable) {
+            return message.channel.send('Io non ho il permesso');
+        }
+        utente.kick()
+            .then(() => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle(`${utente.user.username} kickato`)
+                    .setDescription(`Utente kickato da ${message.author.toString()}`)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
+})
+client.on("messageCreate", message => {
+    if (message.content.startsWith("*clear")) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non hai il permesso');
+        }
+        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            return message.channel.send('Non ho il permesso');
+        }
+        var count = parseInt(message.content.split(/\s+/)[1]);
+        if (!count) {
+            return message.channel.send("Inserisci un numero valido")
+        }
+        if (count > 100) {
+            return message.channel.send("Non puoi cancellare più di 100 messaggi")
+        }
+        message.channel.bulkDelete(count, true)
+        message.channel.send(count + " messaggi eliminati").then(msg => {
+            setTimeout(() => msg.delete(), 5000)
+        })
+    }
 })
 client.on("messageCreate", message => {
     if (message.content.startsWith("*userinfo")) {
@@ -45,74 +129,6 @@ client.on("messageCreate", message => {
             .addField("Joined this server", utente.joinedAt.toDateString(), true)
             .addField("Permissions", elencoPermessi, false)
             .addField("Roles", utente.roles.cache.map(ruolo => ruolo.name).join("\r"), false)
-        message.channel.send({ embeds: [embed] })
-    }
-})
-client.on("messageCreate", message => {
-    if (message.content.startsWith("*clear")) {
-        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
-            return message.channel.send('Non hai il permesso!');
-        }
-        if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) {
-            return message.channel.send('Non ho il permesso');
-        }
-        var count = parseInt(message.content.split(/\s+/)[1]);
-        if (!count) {
-            return message.channel.send("Inserisci un numero valido")
-        }
-        if (count > 100) {
-            return message.channel.send("Non puoi cancellare più di 100 messaggi")
-        }
-        message.channel.bulkDelete(count, true)
-        message.channel.send(count +  " __**Messaggi Cancellati!**__").then(msg => {
-            setTimeout(() => msg.delete(), 5000)
-        })
-    }
-})
-client.on("messageCreate", message => {
-    if (message.content.startsWith("*avatar")) {
-        if (message.content.trim() == "!avatar") {
-            var utente = message.member;
-        }
-        else {
-            var utente = message.mentions.members.first();
-        }
-        if (!utente) {
-            return message.channel.send("Utente non trovato")
-        }
-        var embed = new Discord.MessageEmbed()
-            .setTitle(utente.user.tag)
-            .setDescription("L'avatar di questo utente")
-            .setImage(utente.user.displayAvatarURL({
-                dynamic: true,
-                format: "png",
-                size: 512
-            }))
-        message.channel.send({ embeds: [embed] })
-    }
-})
-client.on("messageCreate", message => {
-    if (message.channel.type == "DM") return
-
-    if (message.member.roles.cache.has("idRuolo1") || message.member.roles.cache.has("idRuolo2")) return
-
-    var parolacce = ["sedia", "lampada", "ciao come va"]
-    var trovata = false;
-    var testo = message.content;
-
-    parolacce.forEach(parola => {
-        if (message.content.toLowerCase().includes(parola.toLowerCase())) {
-            trovata = true;
-            testo = testo.replace(eval(`/${parola}/g`), "###");
-        }
-    })
-
-    if (trovata) {
-        message.delete();
-        var embed = new Discord.MessageEmbed()
-            .setTitle("Hai detto una parolaccia")
-            .setDescription("Hai scritto un messaggio con parole bloccate\rIl tuo messaggio: " + testo)
-
         message.channel.send({ embeds: [embed] })
     }
 })
