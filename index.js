d725e5e887afad753b0b5dcc96850a761ba83c80
Politicooo <97833277+Politicooo@users.noreply.github.com>
2022-03-02 19:39:28 +0100

    Update

--- index.js
+++ index.js
@@ -132,3 +132,31 @@ client.on("messageCreate", message => {
         message.channel.send({ embeds: [embed] })
     }
 })
+client.on("messageCreate", message => {
+    if (message.content.startsWith("*avatar")) {
+        if (message.content.trim() == "!avatar") {
+            var utente = message.member;
+        }
+        else {
+            var utente = message.mentions.members.first();
+        }
+        if (!utente) {
+            return message.channel.send("Utente non trovato")
+        }
+        var embed = new Discord.MessageEmbed()
+            .setTitle(utente.user.tag)
+            .setDescription("L'avatar di questo utente")
+            .setImage(utente.user.displayAvatarURL({
+                dynamic: true,
+                format: "png",
+                size: 512
+            }))
+        message.channel.send({ embeds: [embed] })
+    }
+})
+client.on('ready', () => {
+    client.user.setActivity('Closet Of Gamers', { type: 'LISTENING' }); 
+    client.user.setActivity("Closet Of Gamers", {
+    });
+    client.user.setStatus('online')
+})
